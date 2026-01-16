c44cdf77eb1e85fe88230cdbb3afed3a45accba8
apase95 <noobyhandsome@gmail.com>
2026-01-17 03:16:54 +0700

    RESTORE: src/hook/index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,17 @@
+//Data & State Management
+export { default as useLocalStorage } from "./useLocalStorage";
+export { default as useFetch } from "./useFetch";
+export { default as usePrevious } from "./usePrevious";
+export { default as useToggle } from "./useToggle";
+
+//User Interaction
+export { default as useClickOutside } from "./useClickOutside";
+export { default as useDebounce } from "./useDebounce";
+export { default as useHover } from "./useHover";
+export { default as useCopyToClipboard } from "./useCopyToClipboard";
+
+//System & Utilities
+export { default as useInterval } from "./useInterval";
+export { default as useMediaQuery } from "./useMediaQuery";
+export { default as useWindowSize } from "./useWindowSize";
+export { default as useEventListener } from "./useEventListener";
